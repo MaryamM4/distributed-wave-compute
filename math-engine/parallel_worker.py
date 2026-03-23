54bba0f439c9a196b56c3d3c514c99000c3c0954
Maryam <maryamma@outlook.com>
2026-03-23 11:20:04 -0700

    Just print changes for the workers

--- math-engine/parallel_worker.py
+++ math-engine/parallel_worker.py
@@ -21,28 +21,27 @@ redis_channel = f"{RUN_ID}:{os.environ.get('REDIS_CHANNEL', 'wave_channel')}"
 
 # Helper function for polling redis for values
 # Since multiple parts in program cannot continue without info being published
-def poll_redis_raw(r, key, sleep_interval=0.05, timeout=MAX_HANG_TIME, fail_quietly=False, default=None):
+def poll_redis_raw(r, key, sleep_interval=0.05, timeout=MAX_HANG_TIME, fail_quietly=False, default=None, job_idx=None):
     start = time.time()
-    print(f"[Poll Start] Waiting for key: {key}") # Temp dubeug line. DELETE ME. 
+    #print(f"[J{job_idx}-POLL-START] Waiting for key: {key}") 
     
     while not r.exists(key):
         if timeout is not None and (time.time() - start) > timeout:
             if fail_quietly:
-                print((f"[WARN] Key '{key}' not found in Redis within timeout."))
+                print((f"[J{job_idx}-WARN] Key '{key}' not found in Redis within timeout."))
                 return default
             
             else:
-                raise TimeoutError(f"[ERROR] Key '{key}' not found in Redis within timeout.")
+                raise TimeoutError(f"[J{job_idx}-ERROR] Key '{key}' not found in Redis within timeout.")
             
         time.sleep(sleep_interval)
 
-    print(f"[Poll End] Recieved key: {key}") # Temp dubeug line. DELETE ME. 
+    #print(f"[J{job_idx}-POLL-END] Received key: {key}") 
     return r.get(key) # Do not .decode(), since not always expecting strings/json
 
 # @NOTE: Alternative options to sharing via Redis:
 # B. Compute initial matrix before launching workers, save to object storage (con: shared filesystem or S3 setup, so no).
 # C. Modify the Fortran function to only compute chunk. Favoring this option but am avoiding Fortran edits rn.
-# D. MPI instead of Redis (goes against instructions).
 def get_initial_state(r, job_idx, grid_size):
     init_key = f"{RUN_ID}:initial_matrix"
 
@@ -50,10 +49,12 @@ def get_initial_state(r, job_idx, grid_size):
         matrix = np.zeros((grid_size, grid_size), dtype=np.float64, order="F")
         schrodinger_mod.schrodinger_mod.compute_wave_matrix(matrix=matrix, size_n=grid_size, num_steps=NUM_STEPS, h_bar=H_BAR, mass=MASS) 
         r.set(init_key, matrix.tobytes(), ex=TTL) # Publish initial grid for other workers to use
+        print(f"[J{job_idx}-WRITE] Computed and stored initial grid in Redis.")
         
     else:
-        raw = poll_redis_raw(r, init_key)
+        raw = poll_redis_raw(r, init_key, job_idx=job_idx)
         matrix = np.frombuffer(raw, dtype=np.float64).copy().reshape((grid_size, grid_size))
+        print(f"[J{job_idx}-READ] Retrieved initial grid from Redis.")
     
     return matrix
 
@@ -67,27 +68,35 @@ def get_partition_edges(job_idx, total_jobs, grid_size):
 def deserialize_row(raw):
     return np.frombuffer(raw, dtype=np.complex128) if raw else None
 
-# Removed TTL, but causing memory build-up. @TODO something about it.
 def push_edges(pipe, job_idx, chunk, step):
-    pipe.set(f"{RUN_ID}:worker:{job_idx}:top:{step}", chunk[0, :].tobytes(), ex=TTL)
-    pipe.set(f"{RUN_ID}:worker:{job_idx}:bottom:{step}", chunk[-1, :].tobytes(), ex=TTL)
+    top_key = f"{RUN_ID}:worker:{job_idx}:top:{step}"
+    bot_key = f"{RUN_ID}:worker:{job_idx}:bottom:{step}"
+
+    pipe.set(top_key, chunk[0, :].tobytes(), ex=TTL)
+    pipe.set(bot_key, chunk[-1, :].tobytes(), ex=TTL)
+
+    print(f"[J{job_idx}-WRITE] Step {step}: wrote edges to [{top_key}] and [{bot_key}].")
 
 # Blocks until read
 def pull_neighbor_edges(r, job_idx, total_jobs, step):
     top_neighbor = None
-    bottom_neighbor = None
+    bot_neighbor = None
 
     if job_idx > 0: 
         key = f"{RUN_ID}:worker:{job_idx-1}:bottom:{step}"
-        raw = poll_redis_raw(r, key, timeout=None) 
+        raw = poll_redis_raw(r, key, timeout=None, job_idx=job_idx) 
         top_neighbor = deserialize_row(raw)
+
+        print(f"[J{job_idx}-READ] Step {step}: Received top neighbor from worker {job_idx-1} (key: {key}, shape={top_neighbor.shape}).")
 
     if job_idx < total_jobs - 1:
         key = f"{RUN_ID}:worker:{job_idx+1}:top:{step}"
-        raw = poll_redis_raw(r, key, timeout=None) 
-        bottom_neighbor = deserialize_row(raw)
+        raw = poll_redis_raw(r, key, timeout=None, job_idx=job_idx) 
+        bot_neighbor = deserialize_row(raw)
+        
+        print(f"[J{job_idx}-READ] Step {step}: Received bottom neighbor from worker {job_idx+1} (key: {key}, shape={bot_neighbor.shape}).")
     
-    return top_neighbor, bottom_neighbor
+    return top_neighbor, bot_neighbor
 
 def compute_laplacian(grid):
     # np.roll shifts the grid to easily grab adjacent cells
@@ -106,20 +115,20 @@ def apply_boundary(grid):
     grid[:, 0] = 0
     grid[:, -1] = 0
 
-def trim_ghost_rows(grid, top_neighbor, bottom_neighbor):
+def trim_ghost_rows(grid, top_neighbor, bot_neighbor):
     if top_neighbor is not None:
         grid = grid[1:, :]
-    if bottom_neighbor is not None:
+    if bot_neighbor is not None:
         grid = grid[:-1, :]
     return grid
 
-def build_extended(chunk, top_neighbor, bottom_neighbor):
+def build_extended(chunk, top_neighbor, bot_neighbor):
     rows = []
     if top_neighbor is not None:
         rows.append(top_neighbor)
     rows.append(chunk)
-    if bottom_neighbor is not None:
-        rows.append(bottom_neighbor)
+    if bot_neighbor is not None:
+        rows.append(bot_neighbor)
 
     return np.vstack(rows)
 
@@ -136,12 +145,12 @@ def main():
 
     # Determine current worker's reponsiblity
     start_row, end_row = get_partition_edges(job_idx, total_jobs, grid_size)
-    print(f"JOB {job_idx} START (rows {start_row}-{end_row-1})")
+    print(f"[J{job_idx}-START] Planning {total_steps} steps for rows {start_row}-{end_row-1}.")
 
     # Prepare/get first frame (worker 0 uses Fortran module to compute it)
     matrix = get_initial_state(r, job_idx, grid_size) # Will block until it's avilable
     chunk = matrix[start_row:end_row, :].astype(np.complex128)
-    print(f"Job {job_idx} published/recieved initial matrix.")
+    print(f"Job {job_idx} published/Received initial matrix.")
 
     for step in range(total_steps): # "Animation" loop
         ready_key = f"{RUN_ID}:ready:{step}"
@@ -169,13 +178,12 @@ def main():
                 raise RuntimeError(f'Stuck waiting for "go" at step {step}')
             time.sleep(sleep)
 
-        print(f"Worker {job_idx} pulling neighbors for step {step}") # Temp debug line. DELETE ME
-        top_neighbor, bottom_neighbor = pull_neighbor_edges(r, job_idx, total_jobs, step) 
+        top_neighbor, bot_neighbor = pull_neighbor_edges(r, job_idx, total_jobs, step) 
 
         # [Computation]
-        extended = build_extended(chunk, top_neighbor, bottom_neighbor) #  Edges need to see neighbors for laplacian
+        extended = build_extended(chunk, top_neighbor, bot_neighbor) #  Edges need to see neighbors for laplacian
         laplacian = compute_laplacian(extended)               # Neighbor math: Up + Down + Left + Right - 4*(Center)
-        laplacian = trim_ghost_rows(laplacian, top_neighbor, bottom_neighbor) 
+        laplacian = trim_ghost_rows(laplacian, top_neighbor, bot_neighbor) 
 
         chunk += 1j * laplacian * DT # Update wave
         apply_boundary(chunk)        # Force outer edges of grid to stay at 0 so the wave bounces.
@@ -183,11 +191,11 @@ def main():
         # [Periodically Publish to Redis Server]
         if step % PUBLISH_INTERVAL == 0:
             message = {"worker": job_idx, "step": step, "start_row": start_row, "data": np.abs(chunk).tolist()}
+
             r.publish(redis_channel, json.dumps(message))
+            print(f"[J{job_idx}-PUB] Step {step}: published chunk to channel [{redis_channel}]")
 
-            print(f"Worker {job_idx} Published step {step}")
-
-    print(f"JOB {job_idx} FINISH (rows {start_row}-{end_row-1})")
+    print(f"[J{job_idx}-FIN] {total_steps} steps completed for rows {start_row}-{end_row-1}.")
 
 if __name__ == "__main__":
     main()
